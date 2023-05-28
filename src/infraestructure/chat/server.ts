45ac6c83f8a7e3fba49c63dcf63cd12af9003d9b
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2023-05-28 18:19:25 +0200

    fix:fix issues on chat server

--- src/infraestructure/chat/server.ts
+++ src/infraestructure/chat/server.ts
@@ -1,70 +1,60 @@
-const express =  require('express');
-const http = require('http');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
 const app = express();
 const server = http.createServer(app);
-const socket = require('socket.io');
-const io = socket(server);
+const io = new Server(server);
 
-export function createSocketServer(io){
-const rooms = {};
+interface Room {
+  [roomId: string]: string[]; // Array of socket IDs
+}
 
-io.on('connection', socket => {
-    console.log('connection')
-    /*
-        If a peer is initiator, he will create a new room
-        otherwise if peer is receiver he will join the room
-    */
-    socket.on('join room', roomID => {
-        console.log('join room')
-        if(rooms[roomID]){
-            console.log('RoomId: '+roomID)
-            console.log('SocketID: '+socket.id)
-            // Receiving peer joins the room
-            rooms[roomID].push(socket.id)
-            console.log('rooms[roomID].push(socket.id)  :'+rooms[roomID].push(socket.id))
-        }
-        else{
-            // Initiating peer create a new room
-            console.log('Initiating peer create a new room')
-            rooms[roomID] = [socket.id];
-            console.log('RoomIDelse:    '+roomID);
-        }
+const rooms: Room = {};
 
-        /*
-            If both initiating and receiving peer joins the room,
-            we will get the other user details.
-            For initiating peer it would be receiving peer and vice versa.
-        */
-        const otherUser = rooms[roomID].find(id => id !== socket.id);
-        console.log('OtherUser:  '+otherUser);
-        if(otherUser){
-            socket.emit("other user", otherUser);
-            socket.to(otherUser).emit("user joined", socket.id);
-            console.log('Emited')
-        }
+export function createSocketServer(io: Server) {
+  io.on('connection', (socket) => {
+    console.log('connection');
+
+    socket.on('join room', (roomID) => {
+      console.log('join room');
+
+      if (rooms[roomID]) {
+        console.log('RoomId: ' + roomID);
+        console.log('SocketID: ' + socket.id);
+        rooms[roomID].push(socket.id);
+        console.log('rooms[roomID].push(socket.id): ' + rooms[roomID]);
+      } else {
+        console.log('Initiating peer create a new room');
+        rooms[roomID] = [socket.id];
+        console.log('RoomIDelse: ' + roomID);
+      }
+
+      const otherUser = rooms[roomID].find((id) => id !== socket.id);
+      console.log('OtherUser: ' + otherUser);
+
+      if (otherUser) {
+        socket.emit('other user', otherUser);
+        socket.to(otherUser).emit('user joined', socket.id);
+        console.log('Emitted');
+      }
     });
 
-    /*
-        The initiating peer offers a connection
-    */
-    socket.on('offer', payload => {
-        console.log('Offers connection')
-        io.to(payload.target).emit('offer', payload);
+    socket.on('offer', (payload) => {
+      console.log('Offers connection');
+      io.to(payload.target).emit('offer', payload);
     });
 
-    /*
-        The receiving peer answers (accepts) the offer
-    */
-    socket.on('answer', payload => {
-        console.log('Accepts')
-        io.to(payload.target).emit('answer', payload);
+    socket.on('answer', (payload) => {
+      console.log('Accepts');
+      io.to(payload.target).emit('answer', payload);
     });
 
-    socket.on('ice-candidate', incoming => {
-        console.log('ICE')
-        io.to(incoming.target).emit('ice-candidate', incoming.candidate);
-    })
-});
+    socket.on('ice-candidate', (incoming) => {
+      console.log('ICE');
+      io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+    });
+  });
 
-server.listen(9000, () => console.log("Server is up and running on Port 9000"));
+  server.listen(9000, () => console.log('Server is up and running on Port 9000'));
 }
