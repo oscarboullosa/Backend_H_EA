2f9a175c7c36ff9a1ffb610d0e6e93a1d1fc2459
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2023-05-29 10:55:31 +0200

    fix:fix chat server

--- src/infraestructure/app.ts
+++ src/infraestructure/app.ts
@@ -3,38 +3,45 @@ import express from "express";
 import cors from "cors";
 import dbInit from "./db/mongo";
 import routeUser from "./route/user.route";
-import routeLocation from "./route/location.route"
+import routeLocation from "./route/location.route";
 import routeComment from "./route/comment.route";
-import routePublication from "./route/publication.route"
+import routePublication from "./route/publication.route";
 import routeActivity from "./route/activity.route";
 import routeApplication from "./route/application.route";
-import { deleteLocalFileUser, uploadUser } from "./controller/multer/userMulter.ctrl";
-import { deleteLocalFilePublication, uploadPublication } from "./controller/multer/publicationMulter.ctrl";
-import http from 'http';
+import {
+  deleteLocalFileUser,
+  uploadUser,
+} from "./controller/multer/userMulter.ctrl";
+import {
+  deleteLocalFilePublication,
+  uploadPublication,
+} from "./controller/multer/publicationMulter.ctrl";
+import http from "http";
 import { Server } from "socket.io";
 import { createSocketServer } from "./chat/server";
-
-
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const server=http.createServer(app);
-const io=new Server(server);
+const server = http.createServer(app);
+const io = new Server(server);
 
 createSocketServer(io);
 
 const port = process.env.PORT || 3001;
 
 //app.use(uploadUser.single("photoUser"),routeUser,deleteLocalFileUser as express.RequestHandler);
-app.use(uploadUser.single("photoUser"),routeUser);
+app.use(uploadUser.single("photoUser"), routeUser);
 app.use(routeLocation);
 app.use(routeComment);
-app.use(uploadPublication.single("photoPublication"),routePublication,deleteLocalFilePublication as express.RequestHandler);
+app.use(
+  uploadPublication.single("photoPublication"),
+  routePublication,
+  deleteLocalFilePublication as express.RequestHandler
+);
 app.use(routeActivity);
 app.use(routeApplication);
 
-dbInit().then(()=> console.log("Connection to MongoDB is ready"));
+dbInit().then(() => console.log("Connection to MongoDB is ready"));
 app.listen(port, () => console.log(`Ready on port ${port}`));
--- src/infraestructure/chat/server.ts
+++ src/infraestructure/chat/server.ts
@@ -1,11 +1,10 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 interface Room {
   [roomId: string]: string[]; // Array of socket IDs
 }
@@ -13,48 +12,52 @@ interface Room {
 const rooms: Room = {};
 
 export function createSocketServer(io: Server) {
-  io.on('connection', (socket) => {
-    console.log('connection');
+  console.log("Ando aqui");
+  console.log("io:    " + io);
+  io.on("connection", (socket) => {
+    console.log("connection");
 
-    socket.on('join room', (roomID) => {
-      console.log('join room');
+    socket.on("join room", (roomID) => {
+      console.log("join room");
 
       if (rooms[roomID]) {
-        console.log('RoomId: ' + roomID);
-        console.log('SocketID: ' + socket.id);
+        console.log("RoomId: " + roomID);
+        console.log("SocketID: " + socket.id);
         rooms[roomID].push(socket.id);
-        console.log('rooms[roomID].push(socket.id): ' + rooms[roomID]);
+        console.log("rooms[roomID].push(socket.id): " + rooms[roomID]);
       } else {
-        console.log('Initiating peer create a new room');
+        console.log("Initiating peer create a new room");
         rooms[roomID] = [socket.id];
-        console.log('RoomIDelse: ' + roomID);
+        console.log("RoomIDelse: " + roomID);
       }
 
       const otherUser = rooms[roomID].find((id) => id !== socket.id);
-      console.log('OtherUser: ' + otherUser);
+      console.log("OtherUser: " + otherUser);
 
       if (otherUser) {
-        socket.emit('other user', otherUser);
-        socket.to(otherUser).emit('user joined', socket.id);
-        console.log('Emitted');
+        socket.emit("other user", otherUser);
+        socket.to(otherUser).emit("user joined", socket.id);
+        console.log("Emitted");
       }
     });
 
-    socket.on('offer', (payload) => {
-      console.log('Offers connection');
-      io.to(payload.target).emit('offer', payload);
+    socket.on("offer", (payload) => {
+      console.log("Offers connection");
+      io.to(payload.target).emit("offer", payload);
     });
 
-    socket.on('answer', (payload) => {
-      console.log('Accepts');
-      io.to(payload.target).emit('answer', payload);
+    socket.on("answer", (payload) => {
+      console.log("Accepts");
+      io.to(payload.target).emit("answer", payload);
     });
 
-    socket.on('ice-candidate', (incoming) => {
-      console.log('ICE');
-      io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+    socket.on("ice-candidate", (incoming) => {
+      console.log("ICE");
+      io.to(incoming.target).emit("ice-candidate", incoming.candidate);
     });
   });
 
-  server.listen(9000, () => console.log('Server is up and running on Port 9000'));
+  server.listen(9000, () =>
+    console.log("Server is up and running on Port 9000")
+  );
 }
